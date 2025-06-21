542d048282f32130d9bfe36cecdf9f0e5b10da30
ray-seal <rayseal1986@gmail.com>
2025-06-21 11:21:28 +0100

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home(){
+  return (
+    <div style={{ textAlign: 'centre', marginTop: '2rem' }}>
+<h1>MotoGPs!</h1>
+<p>Your Next.js app app is running </p>
+  </div>
+);
+}
