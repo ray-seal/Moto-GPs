44627430db6cbfd02e4e84a8b8f61ddc7620d478
ray-seal <rayseal1986@gmail.com>
2025-06-21 13:37:34 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,28 @@
+{
+  "name": "motomeet",
+  "version": "0.1.0",
+  "private": true,
+  "scripts": {
+    "dev": "next dev",
+    "build": "next build",
+    "start": "next start",
+    "lint": "next lint"
+  },
+  "dependencies": {
+    "next": "14.1.3",
+    "react": "18.2.0",
+    "react-dom": "18.2.0"
+  },
+  "devDependencies": {
+    "@types/react": "^18.2.47",
+    "@types/node": "^20.11.30",
+    "typescript": "^5.4.5",
+    "tailwindcss": "^3.4.1",
+    "postcss": "^8.4.35",
+    "autoprefixer": "^10.4.17"
+  }
+}
+
 export default function Home() {
   return (
     <div style={{ textAlign: 'center', marginTop: '2rem' }}>
