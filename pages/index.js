1dadc290bbf08d4b5521cde1ced14bde3c2c4666
ray-seal <rayseal1986@gmail.com>
2025-06-21 12:01:44 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ export default function Home(){
   return (
     <div style={{ textAlign: 'centre', marginTop: '2rem' }}>
 <h1>MotoGPs!</h1>
-<p>Your Next.js app app is running </p>
+<p>Welcome to Moto-GPs </p>
   </div>
 );
 }
