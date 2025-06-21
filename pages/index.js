e5a779f1d847925f5704fd97c206ae6ad3e5ce7f
ray-seal <rayseal1986@gmail.com>
2025-06-21 13:31:33 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,3 @@
-export default function Home(){
-  return (
-    <div style={{ textAlign: 'centre', marginTop: '2rem' }}>
-<h1>MotoGPs!</h1>
-<p>Welcome to Moto-GPs </p>
-  </div>
-);
-}
-
-import Script from 'next/script';
-
 export default function Home() {
   return (
     <div style={{ textAlign: 'center', marginTop: '2rem' }}>
