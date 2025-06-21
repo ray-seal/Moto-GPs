bd3355f0f38ff56aabe9efe14c3fcbaec7b48e19
ray-seal <rayseal1986@gmail.com>
2025-06-21 13:23:06 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -20,3 +20,17 @@ export default function Home() {
     </div>
   );
 }
+
+import Link from 'next/link';
+
+export default function Home() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '2rem' }}>
+      <h1>MotoGPs!</h1>
+      <p>Welcome to Moto-GPs</p>
+      <Link href="/gps">
+        <button>Start Navigation</button>
+      </Link>
+    </div>
+  );
+}
