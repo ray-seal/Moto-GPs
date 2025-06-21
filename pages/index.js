ad566a59aa54d8fc68ccbafaaf3c6f901a84b1fa
ray-seal <rayseal1986@gmail.com>
2025-06-21 13:16:09 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -6,3 +6,17 @@ export default function Home(){
   </div>
 );
 }
+
+import Script from 'next/script';
+
+export default function Home() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '2rem' }}>
+      <h1>MotoGPs!</h1>
+      <p>Welcome to Moto-GPs</p>
+
+      {/* Load GPS script after page loads */}
+      <Script src="/gps.js" strategy="afterInteractive" />
+    </div>
+  );
+}
